f14e65861e048e171c376ad6acdc73543d276c28
Ben Fleming <itsbenfleming@gmail.com>
2023-11-05 15:24:56 +0100

    family guy finished

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Family Guy',
-  description: 'Come here and learn more about Family Guy!',
+  description: 'Come here and learn more about Family Guys!',
 }
 
 export default function RootLayout({ children }) {
